d225d71decc01efa4f662a7dff8657424398682d
Jorge Viana <secoviana@gmail.com>
2020-12-31 11:56:24 +0100

    part01 - failing feature test for the 4x4 case

--- test/sudoku/core_test.clj
+++ test/sudoku/core_test.clj
@@ -2,4 +2,18 @@
   (:require [clojure.test :refer :all]
             [sudoku.core :refer :all]))
 
+(def _ nil)
 
+(deftest solver-feature-test
+  (testing "the 4x4 case"
+    (let [empty-puzzle [_ _ _ _
+                        _ _ _ _
+                        _ _ _ _
+                        _ _ _ _]
+          a-possible-solution [1 2 3 4
+                               3 4 1 2
+                               4 1 2 3
+                               2 3 4 1]
+          solutions (solve empty-puzzle)]
+      (is (= 288 (count solutions)))
+      (is (contains? solutions a-possible-solution)))))
