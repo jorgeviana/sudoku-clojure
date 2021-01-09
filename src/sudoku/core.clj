300929d32f9721e6e0ca68c7209bb07b428b6afb
Jorge Viana <secoviana@gmail.com>
2021-01-09 18:14:10 +0100

    part02 - making test pass by splitting logic

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -3,5 +3,10 @@
 (defn NOT-IMPLEMENTED-YET []
   (throw (UnsupportedOperationException. "Not implemented yet!")))
 
+(defn solved? [puzzle]
+  (not (some nil? puzzle)))
+
 (defn solve [puzzle]
-  #{puzzle})
+  (if (solved? puzzle)
+    #{puzzle}
+    #{[1]}))
