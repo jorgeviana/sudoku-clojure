ad3198e9a306251670df02d3b0bd3f8ab2f9d6bc
Jorge Viana <secoviana@gmail.com>
2021-01-17 10:52:42 +0100

    part02 - stubbing improve for the missing two values case and calling it

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -18,8 +18,5 @@
       (= puzzle [_ _ 3 4
                  3 4 1 2
                  4 1 2 3
-                 2 3 4 1]) #{[1 2 3 4
-                              3 4 1 2
-                              4 1 2 3
-                              2 3 4 1]}
+                 2 3 4 1]) #{(improve (improve puzzle))}
       :else #{(improve puzzle)})))
